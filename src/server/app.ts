61fea1a24247bf95917859a12e2ec4543f0bf995
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-22 20:57:17 +0200

    Add handleEndpointNotFount middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import handleHealthCheck from "./middlewares/handleHealthCheck/handleHealthCheck.js";
+import handleErrors from "./middlewares/handleErrors/handleErrors.js";
+import handleEndpointNotFound from "./middlewares/handleEndpointNotFound/handleEndpointNotFound.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use(handleHealthCheck);
+app.get("/", handleHealthCheck);
+
+app.use(handleEndpointNotFound);
+app.use(handleErrors);
 
 export default app;
---
+++ src/server/middlewares/handleEndpointNotFound/handleEndpointNotFound.test.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import handleEndpointNotFound from "./handleEndpointNotFound.js";
+import ServerError from "../../ServerError/ServerError.js";
+
+describe("Given the handleEndpointNotFound middleware", () => {
+  describe("When it receives a next function", () => {
+    const req = {};
+    const res = {};
+    const next = jest.fn();
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test("Then it should call the next function with an error with status code 404", () => {
+      const expectedError = {
+        statusCode: 404,
+      } as Pick<ServerError, "statusCode">;
+
+      handleEndpointNotFound(
+        req as Request,
+        res as Response,
+        next as NextFunction,
+      );
+
+      expect(next).toHaveBeenCalledWith(expect.objectContaining(expectedError));
+    });
+
+    test("Then it should call the next function with an error with message'Endpoint not found'", () => {
+      const expectedError = { message: "Endpoint not found" } as Pick<
+        ServerError,
+        "message"
+      >;
+
+      handleEndpointNotFound(
+        req as Request,
+        res as Response,
+        next as NextFunction,
+      );
+
+      expect(next).toHaveBeenCalledWith(expect.objectContaining(expectedError));
+    });
+  });
+});
