76f426b4082c6cd8fdad01f7cdf1ec6991be2963
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-29 14:17:02 +0200

    Add missing healthCheck endpoint test

---
+++ src/server/__tests__/healthCheck.test.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import app from "../app.js";
+
+describe("Given the GET / endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it respond with status code 200 and a 'OK' message", async () => {
+      const expectedMessage = "OK";
+      const expectedStatusCode = 200;
+
+      const response = await request(app).get("/");
+
+      const { message } = (await response.body) as { message: string };
+
+      expect(message).toBe(expectedMessage);
+      expect(response.status).toBe(expectedStatusCode);
+    });
+  });
+});
