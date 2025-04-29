c9bbbcc6212f0bc8248c5d75dac07f52d8c32b7f
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-29 12:50:59 +0200

    Add deletePost method to PostsController

--- src/post/controller/types.ts
+++ src/post/controller/types.ts
@@ -3,10 +3,15 @@ import { NextFunction, Request, Response } from "express";
 export interface PostControllerStructure {
   getPosts: (req: PostsRequest, res: Response) => void;
   addPost: (req: PostsRequest, res: Response, next: NextFunction) => void;
+  deletePost: (req: PostsRequest, res: Response, next: NextFunction) => void;
 }
 
 export interface PostsQuery {
   pageNumber: string;
 }
 
-export type PostsRequest = Request<object, object, object, PostsQuery>;
+export interface PostsBody {
+  postId: string;
+}
+
+export type PostsRequest = Request<PostsBody, object, object, PostsQuery>;
