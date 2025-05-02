bf700e6597faa35ed7ae31723ca3f0e82497e2c9
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-02 02:07:59 +0200

    Add PostDocumentDto interface

--- src/post/dto/types.ts
+++ src/post/dto/types.ts
@@ -3,3 +3,7 @@ import { PostData } from "../types.js";
 export interface PostDataDto extends Omit<PostData, "publishDate"> {
   publishDate: string;
 }
+
+export type PostDocumentDto = PostDataDto & {
+  _id: string;
+};
